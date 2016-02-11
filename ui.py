245eee70c1290b963f140bf18d964b2bb1672eb7
Lucas Pugliesi <me@fplucas.com>
2016-02-11 09:01:59 -0200

    Espaçamento de mensagens

--- ui.py
+++ ui.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 
 def da_boas_vindas():
+    print('\n\n')
     print('Bem vindo ao jogo da forca. Qual é o seu nome?')
     nome = input()
-    print('Seja bem vindo(a) %s. Vamos jogar!' % nome)
+    print('\n')
+    print('Seja bem vindo(a) %s. Vamos jogar!' % nome.capitalize())
     return nome
 
 def imprime_mascara(palavra):
@@ -13,6 +15,7 @@ def imprime_mascara(palavra):
     print('A palavra contém %s caracteres' % (len(palavra)))
 
 def pede_chute():
+    print('\n')
     print('Entre com um chute ou a palavra inteira:')
     chute = input()
     while not chute:
@@ -20,16 +23,21 @@ def pede_chute():
     return chute
 
 def avisa_que_errou(erro):
+    print('\n')
     print('Desculpe, a letra %s não se encontra na palavra sorteada' % erro)
 
 def da_dica(dica):
+    print('\n')
     print('A dica da palavra chave é %s' % dica)
 
 def acertou_palavra(palavra):
+    print('\n')
     print('Parabéns, a palavra era %s' % palavra)
 
 def errou_palavra(chute):
+    print('\n')
     print('Desculpe, a palavra não é %s' % chute)
 
 def informa_chute_repetido(chute):
+    print('\n')
     print('O caractere %s já foi chutado, tente outro:' % chute)
